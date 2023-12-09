2bab52e57113926d6e788e4ad6a5f7cf2119758c
Trezeno <113196678+Trezeno@users.noreply.github.com>
2023-12-09 22:28:30 +0000

    Functions outlines
    Basic functions and the ideas implemented in code

---
+++ functions.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// Need a function that will print the game board
+void printGameBoard() {
+
+}
+
+// Need a function that will print an informative game board
+void printHelpBoard() {
+
+}
+
+// Need a function to grab player input and return that to a function to place it on the game board
+int getPlayerInput() {
+
+}
+
+// Need a function to place player input on to game board that uses the above function
+void placePlayerInput() {
+  // Uses getPlayerInput()
+}
+
+// Need a function to check if the game has been won by someone and return true if it has to the main game loop
+bool checkGameWinConditions() {
+  // Check Horizontal
+
+  // Check Vertical
+
+  // Check Diagonal
+}
+
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "functions.hpp"
 
 int main() {
   return 0;
